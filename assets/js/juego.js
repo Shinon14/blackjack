ed42233df7af64fff3eadd71a319fe1b1bdd8f2a
Shion <matixubkn.801@gmail.com>
2021-06-08 21:49:15 -0400

    Primeros archivos

---
+++ assets/js/juego.js
@@ -0,0 +1,91 @@
+/**
+ * 2C = Two of Clubs
+ * 2D = Two of Diamons
+ * 2H = Two of Hearts
+ * 2S = Two of Spades
+ */
+
+
+let deck         = [];
+const tipos      =['C', 'D', 'H','S']
+const especiales = ['A', 'J','Q','K']
+
+let puntosJugador = 0,
+    puntosComputadora =0;
+
+//referencias html
+
+const btnPedir = document.querySelector('#btnPedir');
+const puntosHTML = document.querySelectorAll('small');
+const divCartasJugador = document.querySelector('#jugador-cartas')
+// This function make a new deck
+const crearDeck = () => {
+
+    for(let i = 2; i<= 10; i++){
+        for(let tipo of tipos ){
+            deck.push(i + tipo)
+        
+        }
+    }
+    for(let tipo of tipos){
+        for(let esp of especiales){
+            deck.push(esp + tipo)
+        }
+    }
+    deck = _.shuffle(deck);
+     
+    console.log(deck);
+    return deck;
+}
+
+
+crearDeck();
+
+
+// this deck makes me take a card
+
+const pedirCarta = () =>{
+
+if(deck.length === 0){
+    throw 'No hay cartas en el deck';
+
+}
+
+    const carta = deck.pop();
+
+
+    return carta;
+}
+
+const valorCarta =(carta)=>{
+
+    const valor = carta.substring(0,carta.length - 1);
+    return(isNaN(valor ) )?
+            (valor === 'A')? 11 : 10
+            :valor *1;
+
+
+
+
+
+
+}
+
+//eventos
+btnPedir.addEventListener('click',()=>{
+    const carta = pedirCarta();
+    puntosJugador = puntosJugador + valorCarta(carta);
+    puntosHTML[0].innerText = puntosJugador;
+    const imgCarta = document.createElement('img');
+    imgCarta.src= `assets/cartas/${carta}.png`;
+    divCartasJugador.append(imgCarta);
+    imgCarta.classList.add('carta');
+    if (puntosJugador > 21){
+        console.warn('Lo siento mucho, perdiste');
+        btnPedir.disabled = true;
+    }else if (puntosJugador === 21){
+        console.warn('21, Genial!')
+        btnPedir.disabled = true;
+    }
+
+});
